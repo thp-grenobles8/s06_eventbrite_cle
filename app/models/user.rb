6dca2a47011b7eacc91ff861d99534f911be0fcd
kawsay <cc.lle@protonmail.com>
2019-05-06 21:41:40 +0100

    heroku logs plz

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_many :events, dependent: :destroy
 
-  after_create :welcome_send
+  after_create :welcome_send 
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
