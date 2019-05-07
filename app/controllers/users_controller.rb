756b14d4a235de63207a7c3e8f324a36d55d222a
kawsay <cc.lle@protonmail.com>
2019-05-07 19:30:52 +0100

    [todo] user_profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_events_organizing = []
+    @user_events = []
+    events_id = []
+
+    # On cherche les EVENT_ID auquel notre @USER participe
+    # > On insert l'EVENT_ID dans #find() afin d'effectuer une requette sur la table EVENTS
+    @user.attendances.each do |attendance|
+      event_id = attendance.event_id
+      Event.find(event_id).administrator_id == current_user.id ?
+      @user_events_organizing << Event.find(event_id) :
+      @user_events << Event.find(event_id)
+    end
+
+
+
+puts "&&&&&&&&&&&&&&&&&&&&&&&&&& #{@user_events}"
+puts "########################## #{@user_events_organizing}"
+
   end
 end
